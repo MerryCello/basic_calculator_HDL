e874a3195b1aa52b20eed6275ad37660eab496e5
GrumpyCello <kfoniciello@gmail.com>
2021-11-30 11:47:09 -0700

    added memory_x16 to the project

---
+++ basic_calculator.srcs/sources_1/imports/Lab5 - Static_Sequential_Memories/memory.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/01/2021 02:51:00 PM
+// Design Name: 
+// Module Name: Memory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// 8 bit word Memory with default of 2 to-the-power-of 4 memory locations
+module Memory_x16
+    #(
+        parameter addr_bit_size = 4
+    )(
+        inout wire [15:0] data,
+        input wire oe, we, clk, // oe => output enable // we => write enable
+        input wire [(addr_bit_size - 1):0] addr
+    );
+    reg [15:0] mem [0:2**addr_bit_size];
+    reg [15:0] data_temp;
+    
+    initial data_temp = 0;
+    
+    assign data = (oe && !we) ? data_temp : 16'hzzzz;
+    
+    always@(posedge clk) begin 
+        if (we) mem[addr] <= data; 
+        else if (oe) data_temp <= mem[addr]; 
+    end
+endmodule
+    
+    
+    
+    
+    
+    
+    
